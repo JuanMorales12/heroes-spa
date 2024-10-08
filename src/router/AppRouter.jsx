82442ba04203c090b176396f44c508ede80dd93f
Juan M <juandejauretche@gmail.com>
2024-10-08 19:03:00 -0300

    heroes first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
+
+export const AppRouter = () => {
+  return (
+    <>
+      <div className="container">
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/*" element={<HeroesRoutes />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
